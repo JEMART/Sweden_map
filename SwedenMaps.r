c114e15d0a4f3a67db5205bbce184e8f7a480bc2
JEMART <jesper@jemanalytics.se>
2017-02-01 23:09:22 +0100

    Made it easier to use

--- SwedenMaps.r
+++ SwedenMaps.r
@@ -2,7 +2,7 @@
 #the first in´s the name of a Swedish county,e.g. Dalarna and the second the data
 #The function uses the column title of the data as label
 
-swedenMap<-function(x,mypalette="Reds",dir){
+swedenMap<-function(x,mypalette="Reds"){
   
   library(ggplot2)
   library(maptools)
@@ -28,23 +28,22 @@ swedenMap<-function(x,mypalette="Reds",dir){
   merge.shp.coef<-merge(states.shp.f, mydata, by="id", all.x=TRUE)
   final.plot<-merge.shp.coef[order(merge.shp.coef$order), ] 
   n<-names(final.plot)
-  n[n=="NAME_1"]<-"County"
+  n[n=="NAME_1"]<-"Län"
   colnames(final.plot)<-n
+  w<-which(names(final.plot)==yn)
+  
   
   #Plot
   
-  g<-ggplot() +
+  ggplot() +
     geom_polygon(data = final.plot, 
-                 aes(x = long, y = lat, group = group, fill = NKI,label=County), 
+                 aes(x = long, y = lat, group = group, fill = final.plot[,w],label=Län), 
                  color = "black", size = 0.25) + 
     coord_map("")+coord_equal()+
     scale_fill_distiller(name=y, palette = mypalette, trans="reverse",breaks = pretty_breaks(n = 5))+
     #scale_fill_manual(name="My discrete variable", values=c("red", "blue", "green", "yellow"))
     theme_nothing(legend = TRUE)+
     labs(title="")
-  
-  dir<-dir
-  ggsave(g, file = paste(dir,"\\",y,".png",sep=""), width = 6, height = 4.5, type = "cairo-png",dpi=300)
-  
+ 
   
 }
